5a2c0c7822625b13d45cccb3693549ab6c9f0daf
abdelbasir22 <mohamedahmed1400@gmail.com>
2023-10-01 17:06:33 +0300

    first commit

---
+++ lib/features/splash/presentaion/views/splash_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SplaslView extends StatelessWidget {
+  const SplaslView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(),
+    );
+  }
+}
