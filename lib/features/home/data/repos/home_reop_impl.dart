1cb0725be081e183a9642786dbc9eafedde3d242
abdelbasir22 <mohamedahmed1400@gmail.com>
2023-10-13 01:37:45 +0300

    implement fetch similer books

--- lib/features/home/data/repos/home_reop_impl.dart
+++ lib/features/home/data/repos/home_reop_impl.dart
@@ -60,4 +60,31 @@ class HomeRepoImpl extends HomeRepo {
       );
     }
   }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSimilerBooks(
+      {required String category}) async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&q=subject: programing&Sorting=relevance');
+
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServierFailure.fromDioException(e),
+        );
+      }
+
+      return left(
+        ServierFailure(e.toString()),
+      );
+    }
+  }
 }
